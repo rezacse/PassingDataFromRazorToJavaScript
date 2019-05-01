a433797f83cabc610e2ee9f469080bf93b0a5280
shamim reza <reza_cse08@hotmail.com>
2019-05-01 15:32:40 +0600

    first commit

---
+++ PassingDataFromRazorToJavaScript/PassingDataFromRazorToJavaScript/Content/app.js
@@ -0,0 +1,35 @@
+﻿var APP = {};
+(function (plugin) {
+
+    function init(model) {
+        var persons = JSON.parse(model);
+
+        var url = '/PMO/ChangeLog/ChangeLogsAjax';
+        $('#personContainer').DataTable({
+            data: persons,
+            processing: true,
+            paging: false,
+            searching: false,
+            //"ajax": {
+            //    "url": url,
+            //    "type": "POST",
+            //    "datatype": "json",
+            //    'contentType': 'application/json',
+            //    "data": function (d) {
+            //        d.ProjectId = parseInt($('#ProjectId').val());
+            //        d.Status = parseInt($('#Status').val());
+
+            //        return JSON.stringify(d);
+            //    }
+            //},
+            columns: [
+                { data: 'Id', title: 'ID' },
+                { data: 'Name', title: 'Name' },
+                { data: 'Email', title: 'Email' },
+                { data: 'PhoneNumber', title: 'Phone No.'},
+            ]
+        });
+    }
+
+    plugin.init = init;
+})(APP);
---
+++ PassingDataFromRazorToJavaScript/PassingDataFromRazorToJavaScript/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using PassingDataFromRazorToJavaScript.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace PassingDataFromRazorToJavaScript.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = GetPersons();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        private List<Person> GetPersons()
+        {
+            var persons = new List<Person>
+            {
+                new Person { Id = 1, Name = "Person 1", Email = "person1@email.com", PhoneNumber = "1111-111-111" },
+                new Person { Id = 2, Name = "Person 2", Email = "person2@email.com", PhoneNumber = "2222-222-222" },
+                new Person { Id = 3, Name = "Person 3", Email = "person3@email.com", PhoneNumber = "3333-333-333" }
+            };
+            return persons;
+
+        }
+    }
+}
